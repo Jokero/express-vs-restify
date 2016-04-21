5f1e0d6ff08615b5bd7885777858003a04f81592
Dmitry Kirilyuk <Dmitry.Kirilyuk@infobip.com>
2016-04-21 14:43:31 +0300

    Output servers ports

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 require('./express').listen(9000, function() {
-    console.log('Express started');
+    console.log('Express started on port 9000');
 });
 
 require('./restify').listen(9001, function() {
-    console.log('Restify started');
+    console.log('Restify started on port 9001');
 });
